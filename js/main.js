23ef5b0faa0d276724afab3cda38a6e9642ab42f
sxg-surface <ypsugar@163.com>
2019-12-22 11:31:15 +0800

    路径规划，添加jquery

--- js/main.js
+++ js/main.js
@@ -19,11 +19,10 @@ loadmap();
 
 //获取url返回结果
 function urlback(urlroute){
-    var urldata;//{{{
+    var urldata;
     $.ajaxSettings.async = false;  
     $.ajax({
         url: urlroute,
-        contentType: "application/json;charset=uft-8", //{{{
         contentType: "application/json;charset=uft-8", 
         dataType: "json", 
         success: function success(retData) {
@@ -34,16 +33,25 @@ function urlback(urlroute){
             //var dynamicpointcor = []
             for (var i=0;i<reqdata.length; i++){
                 // console.log("reqdata[i][1]:",reqdata[i][1],"reqdata[i][0]:",reqdata[i][0]);
-                var tempcor = wgs84togcj02(reqdata[i][0],reqdata[i][1]);  //坐标加密
-                polylinecor.push(tempcor[0]);
-                polylinecor.push(tempcor[1]);
+                // var tempcor = wgs84togcj02(reqdata[i][0],reqdata[i][1]);  //坐标加密
+                // polylinecor.push(tempcor[0]);
+                // polylinecor.push(tempcor[1]);
+                polylinecor.push(reqdata[i])
             }
             urldata = polylinecor;
         }, 
         error: function error(httpRequest) { 
             console.log("请求失败"); 
-            return false; //}}}
+            return false; 
         } 
-    });//}}}
+    });
     return(urldata);
+}
+
+//路径规划
+function routenav(){
+    var url = "http://121.199.14.136:8989/route?point=31.7809361,117.1863596&point=31.7732777,117.2451694&type=json&locale=zh-CN&vehicle=car&weighting=fastest&points_encoded=false";
+    var urlbacklog = urlback(url);
+    console.log('test!!');
+    console.log(urlbacklog);
 }
